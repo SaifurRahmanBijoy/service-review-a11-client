4a0acf287069784b91054d859c00d32d4e1c88b1
SaifurRahmanBijoy <saifurrahmanbijoy1@gmail.com>
2022-11-08 19:58:54 +0600

    header footer layout added

--- src/layout/Main.js
+++ src/layout/Main.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Footer from "../Pages/Shared/Footer/Footer";
+import Header from "../Pages/Shared/Header/Header";
 
 const Main = () => {
-    return (
-        <div>
-            <Outlet></Outlet>
-        </div>
-    );
+  return (
+    <div>
+      <Header></Header>
+      <Outlet></Outlet>
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default Main;
