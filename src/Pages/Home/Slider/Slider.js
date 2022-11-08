3991e2081ae21f30c144cb874a9ec319adbf927b
SaifurRahmanBijoy <saifurrahmanbijoy1@gmail.com>
2022-11-09 00:08:05 +0600

    slider is completed

--- src/Pages/Home/Slider/Slider.js
+++ src/Pages/Home/Slider/Slider.js
@@ -2,9 +2,15 @@ import React from "react";
 import "./Slider.css";
 
 const Slider = () => {
-  const sliderText = <>
-  <h2 className="text-slate-200 md:p-20 text-sm lg:text-4xl font-semibold font-mono">As a photographer, good feedback motivates you to go out of the way for your clients.</h2>
-  </>;
+  const sliderText = (
+    <>
+      <h2 className="text-slate-200 p-10 md:p-20 text-xs lg:text-4xl font-thin font-mono">
+        <span className="font-extrabold text-sm lg:text-5xl">
+          As a photographer,
+        </span>good feedback motivates me to go out of the way for my clients.
+      </h2>
+    </>
+  );
   return (
     <div className="carousel w-3/4 mx-auto my-6 rounded-xl">
       <div id="slide1" className="carousel-item relative w-full">
@@ -16,9 +22,9 @@ const Slider = () => {
           />
         </div>
         <div className="absolute flex justify-between transform -translate-y-1/2 text-center top-1/2">
-            {sliderText}
+          {sliderText}
         </div>
-        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+        <div className="absolute hidden lg:flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
           <a href="#slide4" className="btn btn-circle text-white btn-outline">
             ❮
           </a>
@@ -36,9 +42,9 @@ const Slider = () => {
           />
         </div>
         <div className="absolute flex justify-between transform -translate-y-1/2 text-center top-1/2">
-            {sliderText}
+          {sliderText}
         </div>
-        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+        <div className="absolute hidden lg:flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
           <a href="#slide1" className="btn btn-circle text-white btn-outline">
             ❮
           </a>
@@ -56,9 +62,9 @@ const Slider = () => {
           />
         </div>
         <div className="absolute flex justify-between transform -translate-y-1/2 text-center top-1/2">
-            {sliderText}
+          {sliderText}
         </div>
-        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+        <div className="absolute hidden lg:flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
           <a href="#slide2" className="btn btn-circle text-white  btn-outline">
             ❮
           </a>
@@ -76,9 +82,9 @@ const Slider = () => {
           />
         </div>
         <div className="absolute flex justify-between transform -translate-y-1/2 text-center top-1/2">
-            {sliderText}
+          {sliderText}
         </div>
-        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+        <div className="absolute hidden lg:flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
           <a href="#slide3" className="btn btn-circle text-white btn-outline">
             ❮
           </a>
