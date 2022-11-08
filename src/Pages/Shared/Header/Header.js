44f65796e1168e55d4dd54bed1b807a1686329e3
SaifurRahmanBijoy <saifurrahmanbijoy1@gmail.com>
2022-11-08 21:57:28 +0600

    header footer layout added

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,11 +1,58 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-    return (
-        <div>
-            <h2>Header</h2>
+  return (
+    <div className="navbar bg-secondary text-white">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost btn-circle btn-xs sm:btn-sm ">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16M4 18h7"
+              />
+            </svg>
+          </label>
+          <ul
+            tabIndex={0}
+            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-primary rounded-box w-52"
+          >
+            <li>
+              <Link>Homepage</Link>
+            </li>
+            <li>
+              <Link>Portfolio</Link>
+            </li>
+            <li>
+              <Link>About</Link>
+            </li>
+            <li>
+              <Link>Profile</Link>
+            </li>
+          </ul>
         </div>
-    );
+      </div>
+      <div className="navbar-center">
+        <Link className="btn btn-ghost upper-case lg:text-xl">
+          Reviews N Critiques
+        </Link>
+      </div>
+      <div className="navbar-end">
+        <button className="btn btn-outline text-white font-normal">
+          <Link>Login</Link>
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
