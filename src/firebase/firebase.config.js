ab8d2e736ca90143bdda3b53b8bd718db71be08b
SaifurRahmanBijoy <saifurrahmanbijoy1@gmail.com>
2022-11-08 19:35:49 +0600

    react-router configured

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBLBoAQ2DkGVWnptAty2EvFz5wkqnrsRps",
+  authDomain: "service-review1.firebaseapp.com",
+  projectId: "service-review1",
+  storageBucket: "service-review1.appspot.com",
+  messagingSenderId: "1066904684814",
+  appId: "1:1066904684814:web:18dba8705770a014e4ef5f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
