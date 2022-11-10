2502907ee78cba66b154891280cef6dda0684cb7
SaifurRahmanBijoy <saifurrahmanbijoy1@gmail.com>
2022-11-10 13:11:51 +0600

    reviews are loaded

---
+++ src/Pages/ServiceAndReview/Review/Review.js
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from "react";
+
+const Review = ({ _id }) => {
+  const [reviews, setReviews] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/reviews")
+      .then((res) => res.json())
+      .then((data) => {
+        // const filteredReview = data.filter((d) => d._id === _id);
+        setReviews(data);
+        console.log(data);
+      });
+  }, [_id]);
+
+  return (
+    <div>
+      {reviews.map((review) => (
+        <div key={review._id} className="card flex items-center justify-center card-side bg-gray-100 my-2 shadow-xl">
+          {review?.userImg ? (
+            <img
+              className="w-16 h-16 mx-5 rounded-full"
+              src={review.userImg}
+              alt=""
+            />
+          ) : (
+            "👤"
+          )}
+          <div className="card-body">
+            <h2 className="card-title text-lg mb-0">{review.user}</h2>
+            <p className="text-md">{review.review}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Review;
--- src/Pages/ServiceAndReview/ServiceById/ServiceById.js
+++ src/Pages/ServiceAndReview/ServiceById/ServiceById.js
@@ -1,10 +1,49 @@
 import React, { useContext } from "react";
 import { Link, useLoaderData } from "react-router-dom";
 import { AuthContext } from "../../../Contexts/AuthProvider/AuthProvider";
+import Review from "../Review/Review";
 
 const ServiceById = () => {
   const { user } = useContext(AuthContext);
   const { _id, img, title, price, about } = useLoaderData();
+  //   ->
+
+  //   -->
+  const handlePlaceReview = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const review = form.review.value;
+    const email = user?.email || "unregistered";
+    const userImg = user?.photoURL || "👤";
+    console.log(review);
+
+    const revDetails = {
+      service: _id,
+      serviceName: title,
+      user: name,
+      userImg: userImg,
+      email,
+      review: review,
+    };
+    fetch("http://localhost:5000/reviews", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(revDetails),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.acknowledged) {
+          alert("Your review has been placed!");
+          form.reset();
+        }
+      })
+      .catch((err) => console.error(err));
+  };
+  //   --<
   return (
     <div>
       <div className="mx-2 mb-20">
@@ -32,49 +71,37 @@ const ServiceById = () => {
           Reviews about this service:
         </h2>
         <div className="w-4/5 lg:w-3/5 mx-auto mb-20">
-          <div className="card flex items-center justify-center card-side bg-gray-100 shadow-xl">
-            <img
-              className="w-16 h-16 mx-5 rounded-full"
-              src="https://placeimg.com/200/280/arch"
-              alt=""
-            />
-            <div className="card-body">
-              <h2 className="card-title text-lg mb-0">Saifur Rahman</h2>
-              <p className="text-md">
-                Click the button to watch on Jetflix app.
-              </p>
-            </div>
-          </div>
+          <Review _id={_id}></Review>
         </div>
         {/* ----< */}
 
         {user ? (
           <div className="w-4/5 lg:w-3/5 mx-auto mb-20">
-            <form>
+            <form onSubmit={handlePlaceReview}>
               <h2 className="text-2xl text-slate-500 font-bold text-center my-5 uppercase">
                 Please drop a review about this service of mine
               </h2>
-              <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 m-3">
-                <input
-                  name="name"
-                  type="text"
-                  placeholder="Name"
-                  className="input input-bordered w-full"
-                  required
-                />
-                <input
-                  name="email"
-                  type="text"
-                  placeholder="Your Email"
-                  // defaultValue={user?.email}
-                  className="input input-bordered w-full"
-                  readOnly
-                />
-              </div>
+              <input
+                name="name"
+                type="text"
+                placeholder="Name"
+                className="input input-bordered w-full"
+                required
+              />
+              <input
+                name="email"
+                type="text"
+                placeholder="Your Email"
+                defaultValue={user?.email}
+                className="input input-bordered w-full"
+                readOnly
+              />
+
               <textarea
-                name="message"
-                className="textarea textarea-bordered w-full "
-                placeholder="Your Message"
+                name="review"
+                type="text"
+                placeholder="Your Review"
+                className="input input-bordered w-full"
                 required
               ></textarea>
               <input
@@ -86,7 +113,11 @@ const ServiceById = () => {
           </div>
         ) : (
           <h2 className="text-2xl text-center my-10">
-            Please <Link className="btn-ghost text-blue-500" to="/login">login</Link> to provide a review.
+            Please{" "}
+            <Link className="btn-ghost text-blue-500" to="/login">
+              login
+            </Link>{" "}
+            to provide a review.
           </h2>
         )}
       </div>
